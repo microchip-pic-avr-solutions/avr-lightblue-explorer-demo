e1108e836f32dfb22c400294e7a4df20f5c6598a
Microchip Technology <microchip@microchip.com>
2020-02-27 22:23:18 +0000

    PAI-296: Updated with fmpp files required to allow generation of source files.;PAI-294: Remove old submodule references prior to adding new.;PAI-294: Add new submodule reference, and application ftl files used for generation.;PAI-295: Updated with pipeline files mirrored that of the pic project, but with updated project name.;PAI-296: Corrected folder location for application ftl files.;PAI-296: Updated submodule reference version for the ble Explorer code;PAI-296: Updated fmpp scripts to refer to AVR as the architectureType;PAI-296: Updated jenkins file for pre-build work.;PAI-296: Fixed typo;PAI-296: Updated main to use int over void. add omit to init function call which is not required for the avr device.;PAI-296: Updated submodule reference to corrected material. main ftl update to remove light blue init.;PAI-296: Corrected passing of USER_PASS where PASS was being used for the token after creation. Added exclude to make portion of code more generic.;PAI-296: Update per comment on pic jenkins file.;PAI-296: Updated cloudprovider to allow for rynch during deployment stage.;PAI-296: Removed un required execute action.;

---
+++ AVR_LIGHTBLUE_EXPLORER_DEMO.X/mcc_generated_files/include/tca0.h
@@ -0,0 +1,56 @@
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef TCA_H_INCLUDED
+#define TCA_H_INCLUDED
+
+#include "../utils/compiler.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief Initialize tca interface
+ *
+ * \return Initialization status.
+ */
+    
+typedef void (*TCA0_cb_t)(void);    
+
+int8_t TCA0_Initialize();
+void TCA0_SetOVFIsrCallback(TCA0_cb_t cb);
+void TCA0_SetCMP0IsrCallback(TCA0_cb_t cb);
+void TCA0_SetCMP1IsrCallback(TCA0_cb_t cb);
+void TCA0_SetCMP2IsrCallback(TCA0_cb_t cb);
+void TCA0_EnableInterrupt(void);
+void TCA0_DisableInterrupt(void);
+uint16_t TCA0_ReadTimer(void);
+void TCA0_WriteTimer(uint16_t timerVal);
+void TCA0_ClearOverflowInterruptFlag(void);
+bool TCA0_IsOverflowInterruptEnabled(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TCA_H_INCLUDED */
